a5995ba972e2794a34dd33c57768d8976f60d35e
João Sanchez <joao.sanchez01@gmail.com>
2025-08-05 01:14:46 -0300

    [Alu-Music] app/models/comentario.py - Ajustando a coluna que recebe as tags_funcionalidades como um JSON, para salvar corretamente no banco PostgreSQL.

--- app/models/comentario.py
+++ app/models/comentario.py
@@ -1,12 +1,14 @@
 from app import db
 import uuid
 
+from sqlalchemy.dialects.postgresql import JSONB
+
 class Comentario(db.Model):
     __tablename__ = "comentarios"
 
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     texto = db.Column(db.Text, nullable=False)
     categoria = db.Column(db.String(20))
-    tags = db.Column(db.Text)
+    tags_funcionalidades = db.Column(JSONB)
     confianca = db.Column(db.Float)
     criado_em = db.Column(db.DateTime, server_default=db.func.now())
